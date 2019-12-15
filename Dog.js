ded934398d7b19b3a74f6355175b9c437eabd8c4
tung519 <vutung959595@gmail.com>
2019-12-15 07:57:08 +0700

    add comments for Dog.js

--- Dog.js
+++ Dog.js
@@ -6,3 +6,29 @@ Dog.prototype.eat = function(cat) {
 	this.stomach.push(cat);
 };
 
+/*
+
+nhớ lại kiến thức:
+
+git checkout -- ten file
+xóa code vừa viết ở trạng thái working directory nhưng phải là file modifiled
+
+git reset HEAD ten file
+chuyển 1 file ở trạng thái staging area về working directory
+
+git checkout -b ten-branch
+tạo ra 1 branch mới đồng thời chuyển sang branch mới vừa tạo
+
+git branch 
+để kiểm tra xem đang ở branch nào
+
+git checkout ten-branch
+để chuyển sang một branch cần chuyển
+
+git merge ten-branch
+để thêm file, thêm code vào branch master (nhớ là phải chuyển về branch master)
+
+git branch -D ten-branch
+xóa brand phụ
+
+*/
